05d413b92835cff29baa291b77f72ed1c75f3d70
Fil <runfilrun@gmail.com>
2019-05-06 20:14:16 +0000

    Adds jQuery development files.  Toggle of sidebar menu for mobile header.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 //respond to click event of mobile menu icon
 
 $(function() {
+    let sideBar = $("#side-menu");
+
+
+
     $("#menu-icon").click(function() {
-        console.log('clicked');
+        let sideMenu = $("#side-menu");
+        sideMenu.toggleClass("mobile-header-view");
+        sideMenu.toggleClass("hide-when-mobile");
+
+        console.log('clicked', sideMenu.hasClass("mobile-header-view"));
     });
 });
