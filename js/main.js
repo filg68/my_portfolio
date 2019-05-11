afd2ebe7d7b115a6430cd17931e5cc1b0b883068
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-05-11 11:25:04 -0400

    Adds basic html for testimonials section.  Moves jquery loading script to page footer.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,21 @@ $(function() {
     let sideMenu = $("#side-menu");
 
     //Initialize the Owl Carousel
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel(
+        {
+            items:2,
+            autoplay:true,
+            autoplayTimeout:5000,
+            responsiveClass:true,
+            loop:true,
+            responsive: {
+                0:{items:1},
+                600:{items:2},
+                1200:{items:3},
+                1600:{items:4},
+                2000:{items:5}
+                }
+        });
 
     //if not in mobile size remove mobile header view if attached
     $( window ).resize(function() {
